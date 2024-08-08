c8313a7ffb0ce082b49f270d37cd3ecd13851837
Joseph50079 <josephogbah2019@gmail.com>
2024-08-08 12:36:10 +0100

    fixing

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -4,7 +4,8 @@
 app.py module for creating Blueprint
 """
 
-from api.v1.views.index import *
 from flask import Blueprint
 
-app_views = Blueprint('app_views', __name__)
+app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
+
+from api.v1.views.index import *
--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 
-"""module app"""
+"""
+Module index for route(s)
+"""
 
 from models.review import Review
 from models.amenity import Amenity
@@ -9,7 +11,7 @@ from models.state import State
 from models.place import Place
 from models.user import User
 from models.base_model import BaseModel
-from . import app_views
+from api.v1.views import app_views
 from flask import Flask, jsonify
 from models import storage
 
@@ -26,16 +28,16 @@ classes = {
 }
 
 
-@app_views.route('/status')
+@app_views.route('/status', methods=['GET'], strict_slashes=False)
 def status():
     """returns status"""
     return (jsonify(j_dict))
 
 
-@app_views.route('/stats')
+@app_views.route('/stats', methods=['GET'], strict_slashes=False:)
 def stats():
     """Get stats of objects of Airbnb"""
     stats_json = {}
     for key, value in classes.items():
         stats_json[key] = storage.count(value)
-    return stats_json
+    return (jsonify(stats_json))
